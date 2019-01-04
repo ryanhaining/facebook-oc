439c71c8dfe47426068b5dbf00fe66f866bdc933
Ryan Haining <haining.cpp@gmail.com>
2019-01-04 02:03:39 -0500

    Changes color for ON/OFF display
    green for on, dark red for off
    
    issue #1

--- background.js
+++ background.js
@@ -1,25 +1,39 @@
-function getOcModeFromResult(result) {
-  return Object.keys(result).length ? result['ocMode'] : true;
+function colorForMode(ocMode) {
+  return ocMode ? "green" : "darkred";
+}
+
+function setColor(ocMode) {
+  chrome.browserAction.setBadgeBackgroundColor({color: colorForMode(ocMode)});
 }
 
 function boolToOnOff(arg) {
-  return arg ? "ON" : "OFF";
+  return arg ? " ON" : "OFF";
 }
 
 function setText(ocMode) {
   chrome.browserAction.setBadgeText({'text': boolToOnOff(ocMode)});
 }
 
+function updateBadge(ocMode) {
+  setText(ocMode);
+  setColor(ocMode);
+}
+
+
+function getOcModeFromResult(result) {
+  return Object.keys(result).length ? result['ocMode'] : true;
+}
+
 chrome.storage.local.get(['ocMode'], function(result) {
   ocMode = getOcModeFromResult(result);
-  setText(ocMode);
+  updateBadge(ocMode);
 });
 
 chrome.browserAction.onClicked.addListener(function() {
   chrome.storage.local.get(['ocMode'], function(result) {
     newOcMode = !getOcModeFromResult(result);
     chrome.storage.local.set({'ocMode': newOcMode},function() {
-      setText(newOcMode);
+      updateBadge(newOcMode);
     });
   });
 });
